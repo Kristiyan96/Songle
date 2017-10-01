4cf60e899cd7353dcf640396cced50573bde78a7
Kristiyan96 <kristiyan.c@gmail.com>
2017-10-01 17:27:22 +0100

    Working links to Instructions and Game activities

--- app/src/main/java/com/moonhythe/songle/MainActivity.java
+++ app/src/main/java/com/moonhythe/songle/MainActivity.java
@@ -1,13 +1,34 @@
 package com.moonhythe.songle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    public void gotoGame(View v){
+        Intent gotoGame = new Intent(this, GameActivity.class);
+        startActivity(gotoGame);
+    }
+
+    public void gotoInstructions(View v){
+        Intent gotoInstructions = new Intent(this, InstructionsActivity.class);
+        startActivity(gotoInstructions);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
