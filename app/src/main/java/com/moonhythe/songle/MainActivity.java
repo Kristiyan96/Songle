c8084920c4c9c03bae0562ff914f659f763266ae
Kristiyan96 <kristiyan.c@gmail.com>
2017-10-29 12:29:13 +0000

    Clean start, MainActivity design, Regenerated History and Game

--- app/src/main/java/com/moonhythe/songle/MainActivity.java
+++ app/src/main/java/com/moonhythe/songle/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+
+    ImageButton start_btn, instructions_btn, history_btn;
 
     public void gotoGame(View v){
         Intent gotoGame = new Intent(this, GameActivity.class);
@@ -30,5 +33,29 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+        start_btn = (ImageButton) findViewById(R.id.start_btn);
+        instructions_btn = (ImageButton) findViewById(R.id.instructions_btn);
+        history_btn = (ImageButton) findViewById(R.id.history_btn);
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        instructions_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(MainActivity.this, InstructionsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        history_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
