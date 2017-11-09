5d04b2bcb98c8bff4c00e3cc14fec28e62a9f7d9
Kristiyan96 <kristiyan.c@gmail.com>
2017-11-09 17:36:45 +0000

    Implemented Levenshtein distance to compare a guess and the title

--- app/src/main/java/com/moonhythe/songle/GameManager/GameData.java
+++ app/src/main/java/com/moonhythe/songle/GameManager/GameData.java
@@ -208,11 +208,64 @@ public class GameData extends Activity {
     }
 
     public void guessSong(String song_title){
-        if(song.getTitle()==song_title){
+        if(closeEnough(song.getTitle(),song_title)){
             // Jump to congratulations screen
+            game.showMessage("Correct", "Congratulations!");
         }else{
             game.showMessage("Wrong", "That's not the song title, Try again.");
         }
+    }
+
+    public Boolean closeEnough(String str1, String str2){
+        str1 = str1.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        str2 = str2.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        CharSequence str1_charS = str1;
+        CharSequence str2_charS = str2;
+        int treshold = 1 + str2.length()/5;
+        int distance = levenshteinDistance(str1_charS, str2_charS);
+        Log.i(TAG, "Treshold is " + treshold + " and distance is " + distance);
+        if(distance<=treshold) return true;
+        else return false;
+    }
+
+    public int levenshteinDistance (CharSequence lhs, CharSequence rhs) {
+        int len0 = lhs.length() + 1;
+        int len1 = rhs.length() + 1;
+
+        // the array of distances
+        int[] cost = new int[len0];
+        int[] newcost = new int[len0];
+
+        // initial cost of skipping prefix in String s0
+        for (int i = 0; i < len0; i++) cost[i] = i;
+
+        // dynamically computing the array of distances
+
+        // transformation cost for each letter in s1
+        for (int j = 1; j < len1; j++) {
+            // initial cost of skipping prefix in String s1
+            newcost[0] = j;
+
+            // transformation cost for each letter in s0
+            for(int i = 1; i < len0; i++) {
+                // matching current letters in both strings
+                int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;
+
+                // computing cost for each transformation
+                int cost_replace = cost[i - 1] + match;
+                int cost_insert  = cost[i] + 1;
+                int cost_delete  = newcost[i - 1] + 1;
+
+                // keep minimum cost
+                newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+            }
+
+            // swap cost/newcost arrays
+            int[] swap = cost; cost = newcost; newcost = swap;
+        }
+
+        // the distance is the cost for transforming all letters in both strings
+        return cost[len0 - 1];
     }
 
     public Lyrics getLyrics() {
