0fecdca0a08b5ea892006f3ede901a36ac0035f9
Kristiyan96 <kristiyan.c@gmail.com>
2017-11-14 06:58:46 +0000

    Removed unnecessary comments

--- app/src/main/java/com/moonhythe/songle/GameManager/GameData.java
+++ app/src/main/java/com/moonhythe/songle/GameManager/GameData.java
@@ -230,39 +230,25 @@ public class GameData extends Activity {
         int len0 = lhs.length() + 1;
         int len1 = rhs.length() + 1;
 
-        // the array of distances
         int[] cost = new int[len0];
         int[] newcost = new int[len0];
 
-        // initial cost of skipping prefix in String s0
         for (int i = 0; i < len0; i++) cost[i] = i;
-
-        // dynamically computing the array of distances
-
-        // transformation cost for each letter in s1
         for (int j = 1; j < len1; j++) {
-            // initial cost of skipping prefix in String s1
             newcost[0] = j;
 
-            // transformation cost for each letter in s0
             for(int i = 1; i < len0; i++) {
-                // matching current letters in both strings
                 int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;
-
-                // computing cost for each transformation
                 int cost_replace = cost[i - 1] + match;
                 int cost_insert  = cost[i] + 1;
                 int cost_delete  = newcost[i - 1] + 1;
 
-                // keep minimum cost
                 newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
             }
 
-            // swap cost/newcost arrays
             int[] swap = cost; cost = newcost; newcost = swap;
         }
 
-        // the distance is the cost for transforming all letters in both strings
         return cost[len0 - 1];
     }
 
@@ -292,7 +278,6 @@ public class GameData extends Activity {
         return continue_game;
     }
 
-
     public int getTotal_time_seconds() {
         return total_time_seconds;
     }
@@ -302,26 +287,17 @@ public class GameData extends Activity {
     }
 
     public void saveGameState(){
-        // Save song number
         Preference.setSharedPreferenceString(context, "song_number", song.getNumber());
-        // Save current combo number
         Preference.setSharedPreferenceInt(context, "current_combo_number", current_combo.getCombo());
-        // Save current combo time left
         Preference.setSharedPreferenceInt(context, "current_combo_time", current_combo.getSeconds_lasting());
-        // Save current combo picked words number
         Preference.setSharedPreferenceInt(context, "current_combo_words_picked", current_combo.getCollected_words());
-        // Save picked placemarks
         Preference.setSharedPreferenceString(context, "picked_placemarks", savePickedPlacemarks());
-        // Save current total time
         Preference.setSharedPreferenceInt(context, "current_total_time", getTotal_time_seconds());
     }
 
     public void applyGameState(){
-        // Game time setup
         setTotal_time_seconds(Preference.getSharedPreferenceInt(context, "current_total_time", 0));
-        // Picked placemarks setup
         importPickedPlacemarks();
-        // Current combo setup
         current_combo = getCombo(Preference.getSharedPreferenceInt(context, "current_combo_number", 1));
         current_combo.setSeconds_lasting(Preference.getSharedPreferenceInt(context, "current_combo_time", 120));
         current_combo.setCollected_words(Preference.getSharedPreferenceInt(context, "current_combo_words_picked", 0));
@@ -340,7 +316,6 @@ public class GameData extends Activity {
         for(Placemark placemark : picked_placemarks){
             picked += placemark.getName() + " ";
         }
-        Log.i(TAG, picked);
         return picked;
     }
 }
